f655150e00057ac06d1b3323c970d1d756d88b5c
sw1029 <yunsw62@gmail.com>
2025-06-19 22:28:56 +0900

    .

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,12 +1,12 @@
 # 고객 정보를 저장하는 SQLAlchemy 모델입니다.
 # app/models/customer.py
-from sqlalchemy import String, Number
+from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
 
 class Customer(Base):
     __tablename__ = "Customer"
-    cno:        Mapped[int]    = mapped_column("cno", Number, primary_key=True)
+    cno:        Mapped[int]    = mapped_column("cno", Integer, primary_key=True)
     name:       Mapped[str]    = mapped_column(String(50))
     passwd:     Mapped[str]    = mapped_column(String(128))   # 해시 저장
     email:      Mapped[str]    = mapped_column(String(100))
